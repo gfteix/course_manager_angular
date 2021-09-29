dcb4053bf5ed90c4176b486af08c5c58f7ec5c48
gf-teixeira <gabriel.fteixeira13@gmail.com>
2021-09-29 20:23:51 -0300

    activating routes when accessing components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { ReplacePipe } from './pipe/replace.pipe';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { RouterModule } from '@angular/router';
 import { Error404Component } from './erro-404/erro-404.component';
+import { CourseInfoComponent } from './courses/course-info.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -15,17 +16,21 @@ import { Error404Component } from './erro-404/erro-404.component';
     StarComponent,
     ReplacePipe,
     NavBarComponent,
-    Error404Component
+    Error404Component,
+    CourseInfoComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     RouterModule.forRoot([
-      { 
-        path: '', redirectTo: 'courses',pathMatch: 'full'
-      },
       {
         path: 'courses', component: CourseListComponent
+      },
+      {
+        path: 'courses/info/:id', component: CourseInfoComponent
+      },
+      { 
+        path: '', redirectTo: 'courses',pathMatch: 'full'
       },
       {
         path: '**', component: Error404Component
