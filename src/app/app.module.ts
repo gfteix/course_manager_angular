e3c8e5d1f6f7060b7c8c5b3bafc03df07adf0aab
gf-teixeira <gabriel.fteixeira13@gmail.com>
2021-10-08 11:17:35 -0300

    core module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,23 +4,24 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { StarComponent } from './shared/component/star/star.component';
 import { ReplacePipe } from './shared/pipe/replace.pipe';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { NavBarComponent } from './core/component/nav-bar/nav-bar.component';
 import { RouterModule } from '@angular/router';
 
 import { HttpClientModule } from '@angular/common/http';
 
 import { Error404Component } from './erro-404/erro-404.component';
 import { CourseModule } from './courses/course-module';
+import { CoreModule } from './core/core.module';
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent,
-    Error404Component,
+    Error404Component
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     CourseModule,
+    CoreModule,
     RouterModule.forRoot([
      
       { 
