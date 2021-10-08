af3a157a658ada7c07e39c6cad0dfd659b401933
gf-teixeira <gabriel.fteixeira13@gmail.com>
2021-10-08 11:08:59 -0300

    adding modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,37 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { CourseListComponent } from './courses/course-list.component';
-import { StarComponent } from './star/star.component';
-import { ReplacePipe } from './pipe/replace.pipe';
+import { StarComponent } from './shared/component/star/star.component';
+import { ReplacePipe } from './shared/pipe/replace.pipe';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { RouterModule } from '@angular/router';
 
 import { HttpClientModule } from '@angular/common/http';
 
 import { Error404Component } from './erro-404/erro-404.component';
-import { CourseInfoComponent } from './courses/course-info.component';
+import { CourseModule } from './courses/course-module';
 @NgModule({
   declarations: [
     AppComponent,
-    CourseListComponent,
-    StarComponent,
-    ReplacePipe,
     NavBarComponent,
     Error404Component,
-    CourseInfoComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     HttpClientModule,
+    CourseModule,
     RouterModule.forRoot([
-      {
-        path: 'courses', component: CourseListComponent
-      },
-      {
-        path: 'courses/info/:id', component: CourseInfoComponent
-      },
+     
       { 
         path: '', redirectTo: 'courses',pathMatch: 'full'
       },
